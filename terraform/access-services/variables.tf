48b80e06af8b7b609ae38a93de173da493e0dfd2
Christoph Zink <chzink@gmx.net>
2026-08-14 13:55:08 +0200

    Set NAT Gateway default state to cold

--- terraform/access-services/variables.tf
+++ terraform/access-services/variables.tf
@@ -1,7 +1,7 @@
 variable "nat_resources_enabled" {
   description = "Controls whether the NAT Gateway and its Public IP are deployed."
   type        = bool
-  default     = true
+  default     = false
 }
 
 variable "nat_associations_enabled" {
